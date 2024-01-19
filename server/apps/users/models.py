1fff5cf14cfd46f37eb70f7f1a3715d6d446233f
jubro <ljh06050605@gmail.com>
2024-01-19 19:38:13 +0900

    [style] gamelist css추가중

--- server/apps/users/models.py
+++ server/apps/users/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     user_id = models.CharField(max_length=16, blank=True)
     password = models.CharField(max_length=16)
-    # nickname = models.CharField(max_length=16, unique=True, blank =False)
+    nickname = models.CharField(max_length=16, unique=True, blank =False)
     score = models.IntegerField(default = 0)
 
     # def __str__(self):
