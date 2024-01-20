0540e6f77f17f43aace40b8e1eb491215a37e305
OH-GITAEK <dhrlxor1512@gmail.com>
2024-01-20 09:53:16 +0900

    [feat] README 노션 주소 추가

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Piro20_CardGame_1
+
+# https://www.notion.so/109146b648614d8fbe267a003cd3278c
--- server/config/settings.py
+++ server/config/settings.py
@@ -158,10 +158,7 @@ AUTHENTICATION_BACKENDS = (
     # 'allauth' specific authentication methods, such as login by e-mail
     'allauth.account.auth_backends.AuthenticationBackend',
 
-    
-    
 )
-
 
 LOGIN_REDIRECT_URL = '/'
 
